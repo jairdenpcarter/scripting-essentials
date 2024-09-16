34e0a0334d45cc5f1bb10eefb3660925a4268771
Jairden <11071423@uvu.edu>
2024-09-15 19:07:48 -0600

    completed lab 1

---
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformController : MonoBehaviour
+{
+    private void Update()
+    {
+        // Move the target GameObject
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
+
+        // Rotate the target GameObject
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    }
+}    
